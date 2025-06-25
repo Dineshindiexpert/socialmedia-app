b9d7ca839ad03ed195ebb5ef7ae86aa84fe377c3
Dinesh <dineshjangra0062@gmail.com>
2025-06-25 22:43:04 +0530

    Final fixes: eventlet monkey patching & user_loader

--- app/__init__.py
+++ app/__init__.py
@@ -5,26 +5,27 @@ from flask_socketio import SocketIO
 import os
 
 db = SQLAlchemy()
-socketio = SocketIO(cors_allowed_origins="*")
 login_manager = LoginManager()
+socketio = SocketIO(cors_allowed_origins="*")
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'supersecretkey'
+    app.config['SECRET_KEY'] = 'your_secret_key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///socialmedia.db'
-    app.config['UPLOAD_FOLDER'] = 'app/static/uploads'
-    app.config['PROFILE_FOLDER'] = 'app/static/avatars'
-    app.config['STORY_FOLDER'] = 'app/static/stories'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-    socketio.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    socketio.init_app(app)
 
-    from app.routes import main
-    app.register_blueprint(main)
+    from app.models import User
 
-    with app.app_context():
-        db.create_all()
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    # import and register routes
+    from app.routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from app import create_app, socketio
+import eventlet
+eventlet.monkey_patch()
+
+from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
