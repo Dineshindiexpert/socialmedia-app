85d49e59eeb1fda84adf3caf11df08ecdefa94ea
Dinesh <dineshjangra0062@gmail.com>
2025-06-26 18:08:00 +0530

    Final fix: eventlet monkey_patch and user_loader with create_app

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,34 @@
+# ⚠️ eventlet monkey_patch MUST happen before importing anything else
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_socketio import SocketIO
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'supersecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///socialmedia.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-socketio = SocketIO(app, async_mode='eventlet')
-
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+socketio = SocketIO(async_mode='eventlet')
+login_manager = LoginManager()
 login_manager.login_view = 'login'
 
-from app.models import User
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'supersecretkey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///socialmedia.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+    db.init_app(app)
+    socketio.init_app(app)
+    login_manager.init_app(app)
 
-from app import routes
+    from app.models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    from app.routes import bp as routes_bp
+    app.register_blueprint(routes_bp)
+
+    return app
--- run.py
+++ run.py
@@ -1,34 +1,6 @@
-### run.py ###
-import eventlet
-eventlet.monkey_patch()
+from app import create_app, socketio
 
-from app import app, socketio
+app = create_app()
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=10000)
-
-
-### app/__init__.py ###
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_socketio import SocketIO
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'supersecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///socialmedia.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-socketio = SocketIO(app, async_mode='eventlet')
-
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-from app.models import User
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-from app import routes
+if __name__ == '__main__':
+    socketio.run(app)
